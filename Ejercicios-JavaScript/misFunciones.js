d7d1f5841110939a4c1c757bce54cc2c5faf74d0
Tomas Cabanilla <2205697@ucc.edu.ar>
2023-06-02 10:04:17 -0300

    ejercicios de JavaScript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,6 +6,7 @@
  * @return Valor que retorna
  */
 function cambiarUnidades(id,valor){
+    //se puede poner var metro,pie,yardas; para no tener que poner cada rato document..... y solo pondria metro=""; y asi respectivamente
     if(isNaN(valor)){
         alert("se ingreso un valor invalido");
         document.lasUnidades.unid_metro.value="";
@@ -28,4 +29,95 @@ function cambiarUnidades(id,valor){
         document.lasUnidades.unid_pie.value=3*valor;
         document.lasUnidades.unid_metro.value=0.9144*valor;
     }
+}
+
+function convertirGR(id){
+    var grad, rad;
+    if(id=="grados"){
+        grad=document.getElementById("grados").value;
+        rad=(grad*Math.PI)/180;
+
+    }else if(id=="radianes"){
+        rad =document.getElementById("radianes").value;
+        grad =(rad*180)/Math.PI
+    }
+    document.getElementById("grados").value=grad;
+    document.getElementById("radianes").value=rad;
+}
+function mostrar_ocultar(valorMO){
+    if(valorMO=="val_mostrar"){
+        document.getElementById("divMO").style.display='block';
+    }else if(valorMO=="val_ocultar"){
+        document.getElementById("divMO").style.display='none';
+        //modificar las propiedades de css
+    }
+}
+function calcularSuma(){
+    var  num1, num2;
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=Number(document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].value=num1+num2;
+
+}
+function cargarWeb(){
+    var cant,unidad,urlComp;
+    cant=document.getElementById("distancia").value;
+    unidad=document.getElementsByName("unidades")[0].value;
+    urlComp="segundaWeb.html#"+cant+"#"+unidad;
+    window.open(urlComp);
+}
+function  cargarResultado(){
+    var urlComp,can,un;
+    urlComp=window.location.href.split("/")[5];
+    can=urlComp.split("#")[1];
+    un=urlComp.split("#")[2];
+    document.getElementById("dist").value=can+" "+un;
+}
+function dibujarCirCuad(){
+    var canvas=document.getElementById("myCanvas");
+    var ctx=canvas.getContext("2d");
+    var xMax=canvas.width;
+    var yMax=canvas.height;
+    var margen=5;
+    ctx.fillStyle="#333899";
+    ctx.fillRect(0+margen,yMax-40-margen,40,40);
+    ctx.arc(xMax/2,yMax/2,20,0,2*Math.PI);
+    ctx.stroke();
+    ctx.fillStyle="#99336b";
+    ctx.fill();
+}
+var bandera;
+function dibujar(event){
+    var canvas=document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX=event.clientX;
+    var posY=event.clientY;
+    console.log(posX,posY);
+
+    canvas.onmousedown=function (){bandera=true};
+    canvas.onmouseup=function (){bandera=false};
+    if(bandera){
+
+        ctx.fillRect(posX,posY,5,5);
+        ctx.fill;
+    }
+
+}
+function limpiarCanvas(){
+    var canvas=document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+    canvas.width=canvas.width;
+}
+
+function dibujarCuadriculado(){
+    var canvas=document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    ctx.moveTo(0,20);
+    ctx.lineTo(1000,600);
+    ctx.strokeStyle="#141a1a";
+    ctx.stroke();
+
+
 }
